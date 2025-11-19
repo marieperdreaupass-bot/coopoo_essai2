87fbe168c5a556bfec0cb457a7c91fe084a0db20
DonTamime <mgn4cdgwtp@privaterelay.appleid.com>
2025-11-19 16:53:45 +0100

    Tous les attributs de quete sont completées

--- src/quete.java
+++ src/quete.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 
 public class quete {
-    protected String question;
-    protected String[] choix;
-    protected int bonneReponse;
-    protected String recompense;
-
-    public quete(String question, String[] choix, int bonneReponse, String recompense) {
-        ArrayList<quete> quetes = new ArrayList<>();
-        quetes.add(new quete("2+2=?", new String[]{"1", "2", "4"},3, "+50PV, +50 degats"));
-
-    }
+    private String name;
+    private String[] choix;
+    private int bonneReponse;
+    private String description;
+    private int niveaurequis;
+    private Monstre bossDeLaQuete; // assure que chaque quête est liée à un ennemi spécifique
+    // que votre méthode de combat devra utiliser.
+    private int experienceGagnee; // Pour la progression du personnage.
+    private String objetRecompense; // gérez des items
+    private boolean estComplete; // Initialisé à false, il devient true après avoir vaincu
+    // le boss et donné la récompense.
 }
+
 
