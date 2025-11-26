421a9ec0ea3e89c07d455e67e111dac1f8f94119
Mounguengui <darcidaviddaniel@gmail.com>
2025-11-26 17:19:51 +0100

    bref

--- src/Mostre/Monstre.java
+++ src/Mostre/Monstre.java
@@ -1,4 +1,59 @@
 package Mostre;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Monstre {
+
+    protected String name;
+    protected int pv;
+    protected int attaque;
+    protected int vitesse;
+    protected int niveau;
+    protected int resistance;
+
+    protected ArrayList<Objects> objets;
+
+    public Monstre() {
+        objets = new ArrayList<>();
+    }
+
+    public void attaquer() {
+        System.out.println(name + " attaque et inflige " + attaque + " dégâts !");
+    }
+
+    public void attaqueSpeciale() {
+        System.out.println(name + " lance une attaque spéciale !");
+        System.out.println("Dégâts infligés : " + (attaque * 1.5));
+    }
+
+    public void rugir() {
+        System.out.println(name + " rugit et intimide l’ennemi !");
+    }
+
+    public void seSoigner(int soin) {
+        pv += soin;
+        System.out.println(name + " récupère " + soin + " PV !");
+    }
+
+    public void utiliserObjet(int index) {
+        if (index >= 0 && index < objets.size()) {
+            Objects obj = objets.get(index);
+            System.out.println(name + " utilise " + obj.getNom() + " !");
+            pv += obj.getEffect();
+            System.out.println("PV actuels : " + pv);
+        } else {
+            System.out.println(name + " n'a pas cet objet !");
+        }
+    }
+
+    public void afficherStats() {
+        System.out.println("===== STATISTIQUES =====");
+        System.out.println("Nom : " + name);
+        System.out.println("PV : " + pv);
+        System.out.println("Attaque : " + attaque);
+        System.out.println("Vitesse : " + vitesse);
+        System.out.println("Niveau : " + niveau);
+        System.out.println("Résistance : " + resistance);
+        System.out.println("========================");
+    }
 }
