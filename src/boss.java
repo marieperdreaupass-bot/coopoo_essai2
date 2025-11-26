130881d42152f96ae2043e80c8312c2c0c854f22
Mounguengui <darcidaviddaniel@gmail.com>
2025-11-26 14:34:17 +0100

    bref

--- src/boss.java
+++ src/boss.java
@@ -1,29 +1,38 @@
 import java.util.ArrayList;
 
-public class boss extends perso {
+public class boss {
+
+    protected String name;
+    protected int pv;
+    protected int attaque;
+    protected int vitesse;
+    protected int niveau;
 
     private final ArrayList<Objet> objets;
     private int resistance;
 
-    public boss(String name) {
-        super(name, 7000, 300, 2);
+    public boss()
+    {name = "Hades";
+        pv= 7000;
+        attaque=300;
+        vitesse = 666;
+        niveau = 666;
+
         this.resistance = 150;
-        this.niveau = 1;
         objets = new ArrayList<>();
         objets.add(new Objet("Potion de soin", 5000));
         objets.add(new Objet("Épée magique", 200));
         objets.add(new Objet("Bouclier", 1000));
     }
 
-    @Override
     public void attaquer() {
         System.out.println(name + " attaque avec puissance !");
-        System.out.println("Dégâts infligés : " + degatsDeBase);
+        System.out.println("Dégâts infligés : " + attaque);
     }
 
     public void attaqueSpeciale() {
         System.out.println(name + " lance une attaque spéciale dévastatrice !");
-        System.out.println("Dégâts infligés : " + (degatsDeBase * 2));
+        System.out.println("Dégâts infligés : " + (attaque * 2));
     }
 
     public void rugir() {
@@ -31,17 +40,17 @@ public class boss extends perso {
     }
 
     public void seSoigner() {
-        PV += 2000;
+        pv += 2000;
         System.out.println(name + " se soigne et récupère 2000 PV !");
-        System.out.println("PV actuels : " + PV);
+        System.out.println("PV actuels : " + pv);
     }
 
     public void utiliserObjet(int index) {
         if (index >= 0 && index < objets.size()) {
             Objet obj = objets.get(index);
             System.out.println(name + " utilise " + obj.getNom() + " !");
-            PV += obj.getEffet();
-            System.out.println("PV actuels : " + PV);
+            pv += obj.getEffet();
+            System.out.println("PV actuels : " + pv);
         } else {
             System.out.println(name + " n'a pas cet objet !");
         }
@@ -50,15 +59,15 @@ public class boss extends perso {
     public void afficherStats() {
         System.out.println("===== STATISTIQUES DU BOSS =====");
         System.out.println("Nom : " + name);
-        System.out.println("PV : " + PV);
-        System.out.println("Dégâts : " + degatsDeBase);
+        System.out.println("PV : " + pv);
+        System.out.println("Dégâts : " + attaque);
         System.out.println("Vitesse : " + vitesse);
         System.out.println("Résistance : " + resistance);
         System.out.println("================================");
     }
 
     public static void main(String[] args) {
-        boss monBoss = new boss("Dragon Noir");
+        boss monBoss = new boss();
 
         monBoss.afficherStats();
         monBoss.rugir();
